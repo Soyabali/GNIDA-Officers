2e066eb5f64c9dfdca92deaff5f9d00b2012ff4a
Soyabali <soyabali64@gmail.com>
2025-09-25 15:26:06 +0530

    Add a Agency code

--- lib/screens/viewimage.dart
+++ lib/screens/viewimage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ImageScreen extends StatelessWidget {
 
   final sBeforePhoto;
+
   const ImageScreen({super.key,required this.sBeforePhoto});
 
   @override
